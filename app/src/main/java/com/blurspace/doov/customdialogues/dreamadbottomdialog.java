67dc1bb03b8c57590e5b5106a96bf18a27d4c8be
Admin <bvkaura@gmail.com>
2021-07-25 07:17:37 +0530

    3rd commit

--- app/src/main/java/com/blurspace/doov/customdialogues/dreamadbottomdialog.java
+++ app/src/main/java/com/blurspace/doov/customdialogues/dreamadbottomdialog.java
@@ -1,10 +1,21 @@
 package com.blurspace.doov.customdialogues;
 
+import android.Manifest;
 import android.app.Dialog;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.CursorJoiner;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,15 +25,59 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 
 public class dreamadbottomdialog extends BottomSheetDialogFragment {
-    @Nullable
+    final int IMAGE_PICK_CODE=1000;
+    final int PERMISSION_CODE=1001;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
        View v=inflater.inflate(R.layout.dreamaddbottomdialog,container,false);
+        ImageView dreamimg=v.findViewById(R.id.dreamsetimg);
+        dreamimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                    if(getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED){
+                        String[] permissions={Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions,PERMISSION_CODE);
+
+                    }
+                    else {
+                        pickImageFromGallery();
+
+                    }
+                }
+            }
+        });
        return v;
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(getActivity(), "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+            }
 
-    public interface dreamadbottomListener {
-        void onSaveClick();
-        void onDeleteClick();
+        }
     }
+
+    
+
+    private void pickImageFromGallery() {
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cr=getActivity().getContentResolver();
+        MimeTypeMap mime= MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+    }
+
 }
+
